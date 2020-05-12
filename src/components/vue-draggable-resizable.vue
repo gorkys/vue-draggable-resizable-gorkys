60220b74a95742e0f5fa90ce7168f72adaabb8b4
king <king@tingtas.com>
2020-05-12 15:38:50 +0800

    Fix 原仓库的#_215

--- src/components/vue-draggable-resizable.vue
+++ src/components/vue-draggable-resizable.vue
@@ -373,6 +373,8 @@ export default {
           (this.dragHandle && !matchesSelectorToParentElements(target, this.dragHandle, this.$el)) ||
           (this.dragCancel && matchesSelectorToParentElements(target, this.dragCancel, this.$el))
         ) {
+          this.dragging = false
+
           return
         }
 
